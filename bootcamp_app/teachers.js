29ff4fb3fb8782454216d57ea8542b9c19b318c4
Jake Lollback <jalollback@gmail.com>
2023-09-29 17:22:28 -0500

    SQL Injection Exercise

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,22 @@ const pool = new Pool({
 
 '%${process.argv[2]}%'
 
-pool.query(`
-SELECT teachers.name as teacher_name, cohorts.name
+
+const queryString = `
+  SELECT teachers.name as teacher_name, cohorts.name
   FROM assistance_requests
   JOIN teachers ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON students.cohort_id = cohorts.id
   GROUP BY teachers.name, cohorts.name
-  HAVING cohorts.name LIKE '${process.argv[2]} || 'JUL02'}';
-`)
+  HAVING cohorts.name LIKE $1;
+`;
+
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`];
+
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
